18dcbc2107c7a95c28c7de167640a38159f1d175
Rad <radspychaj@gmail.com>
2025-01-21 22:26:20 +0000

    Amended .env

--- core/base_settings.py
+++ core/base_settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 SECRET_KEY = config('SECRET_KEY', default='fallback_key')
-ALLOWED_HOSTS = ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', 'raddatasolutions.com']
+ALLOWED_HOSTS = ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1', 'raddatasolutions.com']
 DATABASE_URL = config('DATABASE_URL')
 
 # Application definition
